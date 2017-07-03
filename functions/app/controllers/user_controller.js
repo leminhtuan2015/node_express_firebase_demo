dace3418126ccf5bd99fc115b4ef480083841978
Fuji <fuji@Fujis-iMac.local>
2017-07-03 14:55:11 +0700

    add_user_link

--- functions/app/controllers/user_controller.js
+++ functions/app/controllers/user_controller.js
@@ -1,7 +1,12 @@
 user_controller = {
     index: function(request, response) {
         response.render('user/index', { title: 'Express' })
+    },
+
+    new_user: function(request, response) {
+        response.render('user/new', { title: 'Express' })
     }
+
 }
 
 module.exports = user_controller
